be1ffda3815562cf2f2b18d113eabe9a9ae0fb2b
Ronen Verdi <go7hic@gmail.com>
2015-08-04 23:23:07 -0400

    git URL updated

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A jQuery plugin for advanced cross-browser ellipsis on multiple line content',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/DeDeSt/meteor-dotdotdot.git',
+  git: 'https://github.com/DeDeSt/meteor-jquery-dotdotdot.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
